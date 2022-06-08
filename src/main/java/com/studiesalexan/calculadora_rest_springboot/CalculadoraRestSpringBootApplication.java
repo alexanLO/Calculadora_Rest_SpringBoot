ee03f29e7d395778ebb6dfe6172c02cad9b9a0e6
Alexan Lima <alexanlima1.0@gmail.com>
2022-06-08 09:19:11 -0300

    Primeiro commit: criando projeto

---
+++ src/main/java/com/studiesalexan/calculadora_rest_springboot/CalculadoraRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.studiesalexan.calculadora_rest_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraRestSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studiesalexan/calculadora_rest_springboot/CalculadoraRestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studiesalexan.calculadora_rest_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraRestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
